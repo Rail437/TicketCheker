07a5d46d6ab3794fa1cd3b22361910c093c742a8
RailYunusov <opel808437@gmail.com>
2022-03-10 00:11:33 +0300

    work code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,82 @@
 package main.java;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        Map<Integer, Integer> result = new HashMap();
         try {
-            File file1 = new File("src/resources/ticket/2.png");
-            BufferedImage one = ImageIO.read(file1);
-
-            File file = new File("src/resources/ticket/small.png");
+            File file = new File("src/resources/ticket/203205.png");
             BufferedImage source = ImageIO.read(file);
-            BufferedImage result = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
-            File output = new File("src/resources/ticket/small2.png");
+            List<MyCollable> tasks = new ArrayList<>();
 
-            for (int x = 0; x < source.getWidth(); x++) {
-                for (int y = 0; y < source.getHeight(); y++) {
-                    Color firstPixel = new Color(source.getRGB(x, y));
-                    if (new Color(one.getRGB(0,0)).equals(firstPixel)) {
+            tasks.add(new MyCollable("src/resources/ticket/1.png"));
+            tasks.add(new MyCollable("src/resources/ticket/2.png"));
+            tasks.add(new MyCollable("src/resources/ticket/3.png"));
+            tasks.add(new MyCollable("src/resources/ticket/4.png"));
+            tasks.add(new MyCollable("src/resources/ticket/5.png"));
+            tasks.add(new MyCollable("src/resources/ticket/6.png"));
+            tasks.add(new MyCollable("src/resources/ticket/7.png"));
+            tasks.add(new MyCollable("src/resources/ticket/8.png"));
+            tasks.add(new MyCollable("src/resources/ticket/9.png"));
+            tasks.add(new MyCollable("src/resources/ticket/10.png"));
+            tasks.add(new MyCollable("src/resources/ticket/11.png"));
+            tasks.add(new MyCollable("src/resources/ticket/12.png"));
+            tasks.add(new MyCollable("src/resources/ticket/13.png"));
+            tasks.add(new MyCollable("src/resources/ticket/14.png"));
+            tasks.add(new MyCollable("src/resources/ticket/15.png"));
+            tasks.add(new MyCollable("src/resources/ticket/16.png"));
+            tasks.add(new MyCollable("src/resources/ticket/17.png"));
+            tasks.add(new MyCollable("src/resources/ticket/18.png"));
+            tasks.add(new MyCollable("src/resources/ticket/19.png"));
+            tasks.add(new MyCollable("src/resources/ticket/20.png"));
 
-                        for (int i = 0; i < one.getWidth(); i++) {
-                            for (int j = 0; j < one.getHeight();j++) {
-                                if(x + i < source.getWidth() & y + j < source.getHeight()) {
-                                    if(!new Color(one.getRGB(i,j)).equals(firstPixel)){
-                                        break;
-                                    }
-                                    if (i == 15 && j == 15 & new Color(one.getRGB(i,j)).equals(firstPixel)) {
-                                        Color newColor = new Color(0, 200, 0);
-                                        result.setRGB(i + x, j + y, newColor.getRGB());
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
+            try {
+                List<Future<Integer>> futures = executorService.invokeAll(tasks);
+
+                for (int i = 0; i < 20; i++) {
+                    result.put( i + 1,futures.get(i).get());
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            ImageIO.write(result, "png", output);
+            result.entrySet().stream()
+                    .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+                    .forEach(System.out::println);
         } catch (IOException e) {
             System.out.println("Файл не найден");
         }
+        executorService.shutdown();
     }
+
 }
+/*
+
+    BufferedImage result = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
+    File output = new File("src/resources/ticket/small2.png");
+
+    Color newColor = new Color(0, 200, 0);
+
+            for (int x = 0; x < source.getWidth(); x++) {
+        for (int y = 0; y < source.getHeight(); y++) {
+        result.setRGB(x,y, source.getRGB(x,y));
+        }
+        }
+        ArrayList<Integer> rr = findSubimage(source,one);
+        for (int i = 0; i < rr.size(); i+=2) {
+        result.setRGB(rr.get(i),rr.get(i+1),newColor.getRGB());
+        }
+
+        ImageIO.write(result, "png", output);
+*/
