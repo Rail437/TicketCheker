1b742401dd39e1dd0d6b3effbb0c87b5ce6d651b
RailYunusov <opel808437@gmail.com>
2022-03-09 10:47:22 +0300

    edit main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,29 +16,21 @@ public class Main {
             BufferedImage source = ImageIO.read(file);
             BufferedImage result = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
             File output = new File("src/resources/ticket/small2.png");
-            Color[][] bufferArr = new Color[16][16];
-            for (int i = 0; i < one.getWidth(); i++) {
-                for (int j = 0; j < one.getHeight(); j++) {
-                    bufferArr[i][j] = new Color(one.getRGB(i,j));
-                }
-            }
 
             for (int x = 0; x < source.getWidth(); x++) {
                 for (int y = 0; y < source.getHeight(); y++) {
                     Color firstPixel = new Color(source.getRGB(x, y));
-                    if (bufferArr[0][0].equals(firstPixel)) {
-                        int buffX = x;
-                        int buffY = y;
+                    if (new Color(one.getRGB(0,0)).equals(firstPixel)) {
 
                         for (int i = 0; i < one.getWidth(); i++) {
                             for (int j = 0; j < one.getHeight();j++) {
-                                if(buffX + i < source.getWidth() & buffY + j < source.getHeight()) {
-                                    if(!bufferArr[i][j].equals(firstPixel)){
+                                if(x + i < source.getWidth() & y + j < source.getHeight()) {
+                                    if(!new Color(one.getRGB(i,j)).equals(firstPixel)){
                                         break;
                                     }
-                                    if (i == 15 && j == 15 & bufferArr[i][j].equals(firstPixel)) {
+                                    if (i == 15 && j == 15 & new Color(one.getRGB(i,j)).equals(firstPixel)) {
                                         Color newColor = new Color(0, 200, 0);
-                                        result.setRGB(i + buffX, j + buffY, newColor.getRGB());
+                                        result.setRGB(i + x, j + y, newColor.getRGB());
                                         break;
                                     }
                                 }
